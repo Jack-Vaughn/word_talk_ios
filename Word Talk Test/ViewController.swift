584e88bc6de3fd25d78a85e3d1de49b93fc8f97e
Jack Vaughn <jack.vaughn0523@gmail.com>
2020-04-07 19:17:22 -0400

    Initial Commit

---
+++ Word Talk Test/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Word Talk Test
+//
+//  Created by Jack Vaughn on 4/7/20.
+//  Copyright © 2020 vaughn0523. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Word Talk TestTests/Word_Talk_TestTests.swift
@@ -0,0 +1,34 @@
+//
+//  Word_Talk_TestTests.swift
+//  Word Talk TestTests
+//
+//  Created by Jack Vaughn on 4/7/20.
+//  Copyright © 2020 vaughn0523. All rights reserved.
+//
+
+import XCTest
+@testable import Word_Talk_Test
+
+class Word_Talk_TestTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
